8175496a60ee77c5c225f85544b45f5ab170898c
Jacob Haflett <fivek88@Jacobs-MBP.lan>
2023-07-31 21:58:32 -0400

    I hate life yayy added tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  pages: true
+  css: ['~/assets/css/main.css'],
+  pages: true,
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,8 @@
 <template>
-    <h1>Index page</h1>
+    <div>
+        <h1 class="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+    </div>
+
 </template>
