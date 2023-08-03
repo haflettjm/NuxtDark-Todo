d4e5c6cb5fc1f93c0756f2206309a03d4e7f5cfa
Jacob Haflett <fivek88@Jacobs-MacBook-Pro.local>
2023-08-03 02:07:03 -0400

    basic adding todos to todo array

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,12 @@
 <script setup>
-import{ ref, onMounted} from 'vue'
-const todos = ref(["todo1", "todo2", "todo3"])
+
+    import{ ref, onMounted, reactive} from 'vue'
+    const todos = reactive([])
+    let input = ref("")
+    const addTodo = (input) =>{
+        todos.push(input)
+    }
+
 </script>
 <template>
     <div class="
@@ -16,13 +22,13 @@ const todos = ref(["todo1", "todo2", "todo3"])
             my-8
             justify-center
             bg-slate-700
-            p-8
+            p-4
             w-10/12
             h-10/12
             text-center
             rounded-md
         ">
-            <h1 class="my-2">
+            <h1 class="my-2 text-xl font-bold">
                 Todo App
             </h1>
             <div class="
@@ -30,26 +36,41 @@ const todos = ref(["todo1", "todo2", "todo3"])
                 justify-center
                 space-x-2
             ">
-                <div>
-                    <input v-model="text" placeholder="Enter Todo Here...">
+                <div class="flex-col">
+                    <div>
+                        <input 
+                            class="
+                                text-black
+                                rounded
+                                p-4
+                                " 
+                            v-model="input"
+                            type="text" 
+                            placeholder="Enter Todo Here...">
+                    </div>
+                    <div>
+                        <button 
+                        v-on:click="addTodo(input)"
+                        class="
+                            bg-green-300
+                            text-slate-700
+                            p-4
+                            font-bold
+                            rounded-md
+                            my-8
+                        ">
+                            submit
+                        </button>
+                    </div>
                 </div>
                 <div>
                     <ul>
                         <li v-for="item in todos">
-                            {{item}}
+                            {{ item }}
                         </li>
                     </ul>
-                </div>
-
+                </div>  
             </div>
-            <button class="
-                    bg-yellow-300
-                    p-4
-                    rounded-md
-                    my-8
-                ">
-                    click me!
-            </button>
         </div>
     </div>
 
