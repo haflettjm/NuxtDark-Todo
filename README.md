cfdb886cc0249043076b8c5da82e893c71ca5933
Jacob Haflett <fivek88@Jacobs-MacBook-Pro.local>
2023-08-03 02:44:50 -0400

    Updated Road map on readme

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 ### Simple Nuxt Todo App With Dark Mode
 
+#### Finished:
+    + Ability to add todos
+    + Page updates with todos
+    + basic styling
+
+#### "Todo" haha
+    - Add improved styling and darkmode
+    - Add ability to save todos to server (refreshes every week)
+    - Add Date entered and date needed to be done by
+    - Add Displaying current date.
+    
--- pages/index.vue
+++ pages/index.vue
@@ -51,13 +51,7 @@
                             rounded-lg border 
                             border-gray-300 
                             focus:ring-blue-500 
-                            focus:border-blue-500 
-                            dark:bg-gray-700 
-                            dark:border-gray-600 
-                            dark:placeholder-gray-400 
-                            dark:text-white 
-                            dark:focus:ring-blue-500 
-                            dark:focus:border-blue-500" 
+                            focus:border-blue-500" 
                         placeholder="Enter Todo">
                         </textarea>
                     </div>
